dda66e03491e1c77320a406c91b1636707603687
Patrizia Trudu <patrizia.trudu@bonarea.com>
2024-07-31 17:51:38 +0200

    connexio realitzada correctament

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -102,11 +102,6 @@ async function seedRevenue() {
 }
 
 export async function GET() {
-  return Response.json({
-    /*message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',*/
-  });
-
   try {
     await client.sql`BEGIN`;
     await seedUsers();
@@ -120,4 +115,5 @@ export async function GET() {
     await client.sql`ROLLBACK`;
     return Response.json({ error }, { status: 500 });
   }
+  
 }
